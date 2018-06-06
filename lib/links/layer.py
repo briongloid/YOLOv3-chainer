ac3c4678dc691a4c697eb5c898643ffb0a838854
briongloid <briongloid3952@gmail.com>
2018-06-06 15:28:27 +0900

    update

--- lib/links/layer.py
+++ lib/links/layer.py
@@ -1,5 +1,4 @@
 import numpy as np
-import cupy
 
 import chainer
 import chainercv
--- yolov3_camera.py
+++ yolov3_camera.py
@@ -95,3 +95,6 @@ def main():
     
     cap.release()
     cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
